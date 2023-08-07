7c089b47b1f23aab95b505f2561ee43a326ef386
Gavaskar <gavaskaraeg@gmail.com>
2023-08-07 17:41:19 +0530

    basic ui done

--- todo/src/Pages/Home.jsx
+++ todo/src/Pages/Home.jsx
@@ -1,14 +1,18 @@
 
 import React from 'react';
-
+import Style from '../Styles/Home.module.css';
 function Home({todo}) {
    
     return (
         <>
-      
+            <h1 className={Style.popins}>Tasks</h1>
              {todo.map((todo) => 
            
-                <h1 key={todo.id}>{todo.title}</h1>
+
+              <div className={Style.taskDiv} key={todo.id}>
+                <p>{todo.title}</p>
+                {todo.completed ? <h4 className={Style.taskDone}>done</h4> : <h4 className={Style.taskNotDone}>not Done</h4>}
+                </div>
 
              
              )}
