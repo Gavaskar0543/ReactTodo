73717e6c5abaedf331202b53cad96243a5ea8c29
Gavaskar <gavaskaraeg@gmail.com>
2023-08-18 14:45:47 +0530

    ui done

--- todo/src/Pages/Home.jsx
+++ todo/src/Pages/Home.jsx
@@ -1,90 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faWindowClose, faFingerprint,faPencil } from '@fortawesome/free-solid-svg-icons';
-import NewTask from '../Components/NewTask';
-import { destroyTodos } from '../api';
-import Style from '../Styles/Home.module.css';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrash,
+  faPencil,
+} from "@fortawesome/free-solid-svg-icons";
+import NewTask from "../Components/NewTask";
+import { destroyTodos } from "../api";
+import Style from "../Styles/Home.module.css";
 import { getTodos } from "../api";
-import Loader from '../Components/Loader';
-import { toast } from 'react-toastify';
+import Loader from "../Components/Loader";
+import { toast } from "react-toastify";
 
 function Home() {
   const [todos, setTodos] = useState([]);
-  const [loading,setLoading] = useState(true)
-  const [update,setUpdate] =useState(false);
-let updateTask;
-  const handleUpdate = (id)=>{
+  const [loading, setLoading] = useState(true);
+  const [update, setUpdate] = useState(false);
+  let updateTask;
+  const handleUpdate = (id) => {
     setUpdate(true);
-     updateTask = todos.filter((todoItem) => todoItem._id === id);
-     
-  }
+    updateTask = todos.filter((todoItem) => todoItem._id === id);
+  };
   useEffect(() => {
     const fetchTodos = async () => {
       const response = await getTodos();
-     
 
       if (response.success) {
         setLoading(false);
         setTodos(response.data.data);
-        
-       
       }
     };
     fetchTodos();
   }, [todos]);
 
-  
- 
-
-  if(loading){
-    return <Loader />
+  if (loading) {
+    return <Loader />;
   }
   const DeleteTask = async (id) => {
     const response = await destroyTodos(id);
 
     if (response.success) {
-      console.log('Deleted!');
+      console.log("Deleted!");
       const updatedTodo = todos.filter((todoItem) => todoItem._id !== id);
       setTodos(updatedTodo);
     } else {
-      console.log('Not deleted');
+      console.log("Not deleted");
     }
   };
- 
- 
- 
+
   return (
     <>
-    <div className={Style.todoContainer}>
-      <div>
-      <h1 className={Style.heading} >Todo List App</h1>
-      <NewTask todos={todos} />
-     
-      
-      {update ? <input type="text" / >: <></>}
-      
-      {todos.map((todoItem) => (
-        <div  key={todoItem._id}>
-          <FontAwesomeIcon onClick={() => DeleteTask(todoItem._id)} icon={faTrash} />
-          <p>{todoItem.createdAt.split("T")[0]}</p>
-        <p>{todoItem.title} <FontAwesomeIcon onClick={()=>handleUpdate(todoItem._id)} icon={faPencil} /> </p>
-          {todoItem.completed ? (
-            <h4>
-              <FontAwesomeIcon icon={faFingerprint} />
-            </h4>
-          ) : (
-            <h4 >
-              <FontAwesomeIcon icon={faWindowClose} />
-            </h4>
-          )}
+      <div className={Style.todoContainer}>
+        <div>
+          <h1 className="text-4xl text-center">Todo List App</h1>
+          <NewTask todos={todos} />
+
+          <div className="mt-4 border berder-2 flex-col gap-10">
+            {todos.map((todoItem) => (
+              <div
+                className="flex border border-3 mb-4 h-20 justify-between items-center text-lg capitalize"
+                key={todoItem._id}
+              >
+               <div> <input className="h-5 w-10" type="checkbox" /></div>
+               <div>
+                 <p>{todoItem.title}</p>
+                </div>
+                <div className="flex justify-evenly w-20">
+                <div>  <FontAwesomeIcon style={{color:"red"}} icon={faPencil}/></div>
+                 <div> <FontAwesomeIcon style={{color:"green"}} icon={faTrash}/></div>
+                  </div>
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-      </div>
       </div>
     </>
   );
 }
-
-
 
 export default Home;
